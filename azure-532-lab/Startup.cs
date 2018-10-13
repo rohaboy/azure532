ccb3fd709d4dae5ca4c7aef53764a24832099b26
LINUXHOST\nilesh <nileshaj@mlguptha.com>
2018-10-13 11:26:25 -0400

    initial commit

---
+++ azure-532-lab/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(azure_532_lab.Startup))]
+namespace azure_532_lab
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
